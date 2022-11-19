790a11019bf6d3113a284cb820f38ebd329b56a7
arc-015 <chavanavinash015@gmail.com>
2022-11-19 14:13:57 +0530

    Welcome to the Lambda expression

---
+++ src/com/bridgelabz/lambdaexpresson/RegistrationMethod.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.lambdaexpresson;
+
+public class RegistrationMethod {
+
+    
+}
---
+++ src/com/bridgelabz/lambdaexpresson/UserRegistrationMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.lambdaexpresson;
+
+public class UserRegistrationMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Lambda Expression program");
+
+
+    }
+}
